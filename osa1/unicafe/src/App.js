3e973a2fdba6ebdb25c058a994a1075cf6876200
Jesse Toivanen <jesse.toivanen2@gmail.com>
2021-11-29 15:40:22 +0200

    1.11: unicafe step6 tehty.

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -12,19 +12,46 @@ const Statistics = ({ good, neutral, bad }) => {
 
   return (
     <div>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='average' value={(good - bad) / 
-        (good + neutral + bad)} />
-      <StatisticLine text='positive' value={100 * good / 
-        (good + neutral + bad)} unit='%' />
+      <StatisticTable good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
 
-const StatisticLine = ({ text, value, unit }) =>
-  <div>{text} {value} {unit}</div>
+const StatisticTable = ({ good, neutral, bad }) => {
+  const average = (good - bad) / (good + neutral + bad)
+  const positive = 100 * good / (good + neutral + bad)
+  
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>good</td>
+          <td>{good}</td>
+        </tr>
+        <tr>
+          <td>neutral</td>
+          <td>{neutral}</td>
+        </tr>
+        <tr>
+          <td>bad</td>
+          <td>{bad}</td>
+        </tr>
+        <tr>
+          <td>all</td>
+          <td>{good + neutral + bad}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{average.toFixed(1)}</td>
+        </tr>
+        <tr>
+          <td>positive</td>
+          <td>{positive.toFixed(1)} %</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
 
 const App = () => {
   // tallenna napit omaan tilaansa
