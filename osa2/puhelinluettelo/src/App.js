157a6e47e661f3c6b99c6b15c516cf8ccef554ff
Jesse Toivanen <jesse.toivanen2@gmail.com>
2021-12-02 16:04:40 +0200

    2.9: puhelinluettelo step4 tehty.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,14 +1,32 @@
 import React, { useState } from 'react'
 
-const Person = ({ person }) => 
-  <p>{person.name} {person.number}</p>
+const Person = ({ person }) => <p>{person.name} {person.number}</p>
 
+const ListPersons = ({ persons }) =>
+    persons.map(person => 
+      <Person key={person.name} person={person} />)
+
+/*const Filter = ({ persons }) => {
+  
+  return (
+    <div>
+      filter shown with
+      <input 
+    </div>
+  )
+}*/
+  
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244'}
+    { name: 'Arto Hellas', number: '040-1231244'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -32,9 +50,25 @@ const App = () => {
   const handleNumberChange = (event) =>
     setNewNumber(event.target.value)
 
+  const handleFilterChange = (event) => 
+    setFilter(event.target.value)
+
+  const personsShown = (filter === '')
+    ? persons
+    : persons.filter(person => 
+        person.name.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+        value={filter}
+        onChange={handleFilterChange}
+      />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: 
@@ -55,9 +89,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => 
-        <Person key={person.name} person={person} />
-      )}
+      <ListPersons persons={personsShown} />
     </div>
   )
 
