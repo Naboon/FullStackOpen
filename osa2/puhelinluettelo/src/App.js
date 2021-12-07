4a939205d6c0ae5327e7ec1ac29f7a2bd378acaa
Jesse Toivanen <jesse.toivanen2@gmail.com>
2021-12-07 12:32:44 +0200

    2.11: puhelinluettelo step6 tehty.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,20 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import ListPersons from './components/ListPersons'
 import PersonForm from './components/PersonsForm'
 import Filter from './components/Filter'
 
   
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231244'},
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -66,7 +69,6 @@ const App = () => {
       <ListPersons persons={personsShown} />
     </div>
   )
-
 }
 
 export default App
